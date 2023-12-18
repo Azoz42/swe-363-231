3a40c5d5d49987ee4c0ce3141a9ce3779bbe6aa3
Azoz42 <azoz200002@gmail.com>
2023-12-19 00:23:42 +0300

    Js 2 Exercises

--- Swe363/My blog/gamescript.js
+++ Swe363/My blog/gamescript.js
@@ -44,6 +44,23 @@ function handleLose() {
 document.addEventListener("mousemove", e => {
     playerPaddle.position = (e.y / window.innerHeight) * 100
 })
+// js 2
 
 window.requestAnimationFrame(update)
 
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+    
+    const step = 10; 
+
+    switch (event.key) {
+        case "ArrowUp":
+            playerPaddle.position = Math.max(0, playerPaddle.position - step);
+            break;
+        case "ArrowDown":
+            playerPaddle.position = Math.min(100, playerPaddle.position + step);
+            break;
+
+    }
+}
