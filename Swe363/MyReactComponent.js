9e285062f132972f2c67355d29db972e03dab9cd
Azoz42 <azoz200002@gmail.com>
2023-12-19 00:56:18 +0300

    JS 5 Exercises

---
+++ Swe363/MyReactComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyReactComponent = () => {
+  return (
+    <div>
+      <h2>This is a React Component!</h2>
+      <p>It's integrated into an existing website.</p>
+    </div>
+  );
+};
+
+export default MyReactComponent;
---
+++ Swe363/index1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyReactComponent from './MyReactComponent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyReactComponent />
+  </React.StrictMode>,
+  document.getElementById('react-root')
+);
